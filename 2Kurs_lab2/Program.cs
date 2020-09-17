f967cb17a24abaff554a065de79c5ddb53ab26a9
BLABLABLA <vadimca1.boyarchuc@gmail.com>
2020-09-17 15:08:52 +0300

    End code  (It needs to be rewritten (Features_flat))

--- 2Kurs_lab2/Program.cs
+++ 2Kurs_lab2/Program.cs
@@ -20,61 +20,66 @@ namespace _2Kurs_lab2
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.Clear();
             }
-            bool ShowMenu = true;
-            while (ShowMenu)
-            {
-                ShowMenu = MainMenu();
-            }
+
+
+
+            MainMenu();
+
 
         }
-        private static bool MainMenu()
+        public static void MainMenu()
         {
             Task_ t = new Task_();
             GasComp gasComp = new GasComp();
-           
             Console.Clear();
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1. Task 1.1");
-            Console.WriteLine("2. Task 1.2");
-            Console.WriteLine("3. Task 2.1");
-            Console.WriteLine("4. Task 2.2");
-            Console.WriteLine("5. Task 3");
-            Console.WriteLine("6. Task 4");
-            Console.WriteLine("7. Exit");
-            Console.Write("\r\nSelect an option: ");
-            switch (Console.ReadLine())
+            int i = 0;
+            do
             {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Task 1.1");
+                Console.WriteLine("2. Task 1.2");
+                Console.WriteLine("3. Task 2.1");
+                Console.WriteLine("4. Task 2.2");
+                Console.WriteLine("5. Task 3");
+                Console.WriteLine("6. Task 4");
+                Console.WriteLine("7. Exit");
+                Console.Write("\r\nSelect an option: ");
+                switch (Console.ReadLine())
+                {
 
-                case "1":
-                    t.Task_1_1();
-                    return true;
-                case "2":
-                    t.Task_1_2();
-                    return true;
-                case "3":
-                    t.Task_2_1();
-                    return true;
-                case "4":
-                    t.Task_2_2();
-                    return true;
-                case "5":
-                    t.Task_3();
-                    return true;
-                case "6":
-                    gasComp.Fu();
-                    return true;
-                case "7":
-                    return false;
-                default:
-                    Console.WriteLine("Error, wrong menu position! TRY AGAIN");
-                    return true;
+                    case "1":
+                        t.Task_1_1();
+                        break;
+                    case "2":
+                        t.Task_1_2();
+                        break;
+                    case "3":
+                        t.Task_2_1();
+                        break;
+                    case "4":
+                        t.Task_2_2();
+                        break;
+                    case "5":
+                        t.Task_3();
+                        break;
+                    case "6":
+
+                        gasComp.Fu();
+
+                        break;
+                    case "7":
+                        i = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Error, wrong menu position! TRY AGAIN");
+                        break;
 
 
 
-            }
+                }
 
 
-
+            } while (i != 1);
 
         }
     }
@@ -104,7 +109,7 @@ namespace _2Kurs_lab2
             Console.WriteLine("index first :" + index2);
             int count = index2 - index1;
             count *= -1;
-            Console.WriteLine("COunt " + count);
+            Console.WriteLine("Count " + count);
             var mySegmentsArray = new ArraySegment<int>(arr, index2, count);
             int s = 0;
             s -= arr[index2];
@@ -114,7 +119,7 @@ namespace _2Kurs_lab2
                 s += list[ctr];
                 Console.WriteLine(list[ctr]);
             }
-            Console.WriteLine("sum ;;;;;" + s);
+            Console.WriteLine("Sum :" + s);
         }
 
         public void Task_1_2()
@@ -162,7 +167,7 @@ namespace _2Kurs_lab2
             row = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter number of columns : " + column);
             column = Convert.ToInt32(Console.ReadLine());
-            int[,] matrix = new int[row,column];
+            int[,] matrix = new int[row, column];
             sumRows(matrix, row, column);
 
 
@@ -176,7 +181,7 @@ namespace _2Kurs_lab2
             {
                 for (int j = 0; j < column; ++j)
                 {
-                    tempMatrix[i,j] = random.Next(Min, Max);
+                    tempMatrix[i, j] = random.Next(Min, Max);
                 }
 
             }
@@ -185,7 +190,7 @@ namespace _2Kurs_lab2
                 for (int j = 0; j < column; ++j)
                 {
 
-                    Console.WriteLine("Array : " + tempMatrix[i,j]+"\n");
+                    Console.WriteLine("Array : " + tempMatrix[i, j] + "\n");
 
                 }
             }
@@ -195,12 +200,12 @@ namespace _2Kurs_lab2
                 int checkPositivity = 1;
                 for (int j = 0; j < row; ++j)
                 {
-                    sum += tempMatrix[i,j];
-                    if (tempMatrix[i,j] < 0)
+                    sum += tempMatrix[i, j];
+                    if (tempMatrix[i, j] < 0)
                         checkPositivity = 0;
                 }
                 if (checkPositivity == 1)
-                    Console.WriteLine("Sum : " + sum); 
+                    Console.WriteLine("Sum : " + sum);
             }
 
 
@@ -226,7 +231,7 @@ namespace _2Kurs_lab2
             {
                 for (j = 0; j < n; j++)
                 {
-                    Console.WriteLine("Array : "+a[i,j] +"\n");
+                    Console.WriteLine("Array : " + a[i, j] + "\n");
                 }
             }
             for (j = 0; j < (2 * n); j++)
@@ -237,17 +242,17 @@ namespace _2Kurs_lab2
             {
                 for (j = 0; j + i < n; j++)
                 {
-                    s[j] += a[i,i + j];
-                    s[n + j] += a[i + j,i];
+                    s[j] += a[i, i + j];
+                    s[n + j] += a[i + j, i];
                 }
             }
             for (j = 0; j < n; j++)
             {
-                Console.WriteLine("S:"+s[j]);
+                Console.WriteLine("S:" + s[j]);
             }
             for (j = n + 1; j < (2 * n); j++)
             {
-                Console.WriteLine("s[j]"+s[j]);
+                Console.WriteLine("s[j]" + s[j]);
             }
             maxsum = -1000;
             for (j = 1; j < n; j++)
@@ -263,11 +268,11 @@ namespace _2Kurs_lab2
 
         public void Task_3()
         {
-           
+
             string str = "AaEeIiYyOoUu";
             String text = "TExt          teeeeeet eaaaar e te tet et";
             text = text.Trim(new char[] { ',', '.' });
-            string[] textArray = text.Split(new char[] {' ' });
+            string[] textArray = text.Split(new char[] { ' ' });
             Console.WriteLine("Words num:" + textArray.Length);
 
             int cntWords = textArray.Length;
@@ -287,14 +292,14 @@ namespace _2Kurs_lab2
                 if (counters[i] == maxB)
                     Console.WriteLine(textArray[i]);
 
-            while (text.Contains("  ")) 
-                text = text.Replace("  ", " "); 
+            while (text.Contains("  "))
+                text = text.Replace("  ", " ");
 
             Console.WriteLine("Without spacies: " + text);
 
 
         }
-      
+
 
     }
     class GasComp : Flat
@@ -302,42 +307,27 @@ namespace _2Kurs_lab2
         Flat flat = new Flat();
         public void Fu()
         {
-            
-            flat.Funcc();
+            flat.Features_flat();
+            Service();
         }
         public void Service()
         {
-            Console.WriteLine("Next service will  be 20 10 2020");
-            Console.WriteLine("If have a problem with gas press Enter numberflat");
-            string numb = Console.ReadLine();
-            for(int i=0;i<b.Length;i++)
-            {
-                if (b[i].Item4 == numb)
-                {
-                    Console.WriteLine("This weak we will fix your problem ");
-                }
-                else Console.WriteLine("Your flat not exist");
-
-            }
+            
 
 
         }
-
-
-
-
     }
     class Flat
     {
-       
-        private string number_flat ;
+
+        private string number_flat;
         private double count_gas;
         private int count_people;
         private bool counter;
         private string name_owner;
         private int Count_number;
         private decimal price;
-        public Flat(string name_owner="Vasyan", int Count_number=3434, decimal price=22,string number_flat = "124A", double count_gas = 30, int count_people = 2, bool counter = true) 
+        public Flat(string name_owner = "Vasyan", int Count_number = 3434, decimal price = 22, string number_flat = "124A", double count_gas = 30, int count_people = 2, bool counter = true)
         {
             this.number_flat = number_flat;
             this.count_gas = count_gas;
@@ -347,86 +337,120 @@ namespace _2Kurs_lab2
             this.Count_number = Count_number;
             this.price = price;
         }
-       string Get_number_flat()
+        string Get_number_flat()
         {
             return this.number_flat;
 
         }
-       public Tuple<string, int, double, string, double, int, int>[] b;
-        
-        public void Funcc()
+        public Tuple<string, int, double, string, double, int, int>[] b;
+
+        public void Features_flat ()
         {
-            Console.WriteLine("Enter count of new flat");
-            int f = Convert.ToInt32(Console.ReadLine());
-         
-         
-            Console.WriteLine("========================");
-            Console.WriteLine("Enter name owner ");
-            string name_owner = Console.ReadLine();
-            Console.WriteLine("Enter number of counter");
-            int Count_number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter price");
-            double price = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter number flat");
-            string number_flat = Console.ReadLine();
-            Console.WriteLine("Enter count gas");
-            double count_gas = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter count people");
-            int count_people = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 1 if you have the counter. \n Enter 2 if you don't have the counter");
-            int counter = Convert.ToInt32(Console.ReadLine());
-            double pricc=0;
-            if (counter == 1)
+            Console.WriteLine("Enter position menu");
+            Console.WriteLine("1.Add flat");
+            Console.WriteLine("2.Exit");
+            int m = Convert.ToInt32(Console.ReadLine());
+            switch (m)
             {
-                pricc = count_gas;
+
+                case 1:
+                    {
+                        Console.WriteLine("Enter count of new flat");
+                        int f = Convert.ToInt32(Console.ReadLine());
+
+                      
+                        Console.WriteLine("========================");
+                        Console.WriteLine("Enter name owner ");
+                        string name_owner = Console.ReadLine();
+                        Console.WriteLine("Enter number of counter");
+                        int Count_number = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter price");
+                        double price = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter number flat");
+                        string number_flat = Console.ReadLine();
+                        Console.WriteLine("Enter count gas");
+                        double count_gas = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter count people");
+                        int count_people = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter 1 if you have the counter. \n Enter 2 if you don't have the counter");
+                        int counter = Convert.ToInt32(Console.ReadLine());
+                        double pricc = 0;
+                        if (counter == 1)
+                        {
+                            pricc = count_gas;
+                        }
+                        else if (counter == 2)
+                        {
+                            pricc = count_people + (count_gas / 2);
+                        }
+                        else Console.WriteLine("You should enter trueth info about counter");
+                        // Tuple<string, int, decimal, string, double, int, bool> tuple = new Tuple<string, int, decimal, string, double, int, bool>(name_owner, Count_number, price, number_flat, count_gas, count_people, counter);
+                        var list = new List<Tuple<string, int, double, string, double, int, int>>();
+                        for (int i = 0; i < f; i++)
+                        {
+                            list.Add(Tuple.Create(name_owner, Count_number, price * pricc, number_flat, count_gas, count_people, counter));
+                            b = list.ToArray();
+                           
+                        }
+
+                      
+
+                        Console.WriteLine("Print flat:");
+                        for (int i = 0; i < b.Length; i++)
+                        {
+                            Console.WriteLine("Flat :" + b[i] + "\n");
+                        }
+                        Console.WriteLine("Next service will  be 20 10 2020");
+                        Console.WriteLine("Enter position menu");
+                        Console.WriteLine("1.If have a problem with gas");
+                        Console.WriteLine("2.Exit");
+                        int menu = Convert.ToInt32(Console.ReadLine());
+
+                        switch (menu)
+                        {
+                            case 1:
+                                {
+                                    Console.WriteLine("Enter numberflat");
+                                    string numb = Console.ReadLine();
+                                    for (int i = 0; i < b.Length; i++)
+                                    {
+                                        if (b[i].Item4 == numb)
+                                        {
+                                            Console.WriteLine("This weak we will fix your problem ");
+                                            Console.WriteLine("Check");
+                                            Console.WriteLine("Your flat :" + b[i].Item4);
+                                            Console.WriteLine("Name employee : JAMS");
+                                            Console.WriteLine("Price :333");
+
+                                        }
+                                        else Console.WriteLine("Your flat not exist");
+
+
+                                    }
+                                }
+                                break;
+
+                            case 2:
+                                break;
+                            default:
+                                Console.WriteLine("Wring position menu");
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    break;
+                
             }
-            else if (counter == 2)
-            {
-                pricc = count_people + (count_gas / 2);
-            }
-            else Console.WriteLine("You should enter trueth info about counter");
-            // Tuple<string, int, decimal, string, double, int, bool> tuple = new Tuple<string, int, decimal, string, double, int, bool>(name_owner, Count_number, price, number_flat, count_gas, count_people, counter);
-            var list = new List<Tuple<string, int, double, string, double, int, int>>();
-
-            list.Add(Tuple.Create(name_owner, Count_number, price*pricc, number_flat, count_gas, count_people, counter));
-           
-
-            b = list.ToArray();
-
-
-
-            void print_flat()
-            {
-                for (int i = 0; i < b.Length; i++)
-                {
-                    Console.WriteLine("EE :" + b[i] + "\n");
-                }
-            }
-           
-           
         }
-       
-
-
-    }
-    //class Owner: Flat
-    //{
-    //  private int id; 
-    //  private string name;
-    //  private int Count_number;
-    //  private decimal price;
-
-     
-    //    public Owner(string name, int Count_number , decimal price,string number_flat,double count_gas,int count_people, bool counter):base(number_flat, count_gas, count_people, counter)
-    //    {
-    //        this.name = name;
-    //        this.Count_number = Count_number;
-    //        this.price = price;
-    //    }
-
-
-
-    //}
-
-
+    }   
 }
+
+        
+    
+
+    
+   
+
+
+
